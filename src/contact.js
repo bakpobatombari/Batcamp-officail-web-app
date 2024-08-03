aa813792382eba0a72c2fa870990fe532d420548
bakpobatombari <batombari12@gmail.com>
2024-08-03 15:54:23 +0100

    updaet

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,7 @@
 const Contact = () => {
     return (  
         <>
+         <iframe id="JotFormIFrame-242154441009548" title="Batcamp Contact Form" onload="window.parent.scrollTo(0,0)" allowtransparency="true" allow="geolocation; microphone; camera; fullscreen" src="https://form.jotform.com/242154441009548" frameborder="0" style="min-width:100%;max-width:100%;height:539px;border:none;" scrolling="no" > </iframe> <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script> <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-242154441009548']", "https://form.jotform.com/")</script> 
         </>
     );
 }
